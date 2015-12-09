9ff31265db990839552984fbdde887bcdcbd6402
indiependente <rockerg991@gmail.com>
2015-12-09 15:36:06 +0100

    dendrogram

--- DTW More Songs.py
+++ DTW More Songs.py
@@ -10,12 +10,13 @@
 import os
 import sys
 from extract_chroma import read_pitches
+from extract_chroma import read_sample
 from random import sample
 import hdf5_getters
 import numpy as np
 from dtw import dtw
 from numpy.linalg import norm
-
+import cPickle as pickle
 #matplotlib.rcParams['figure.figsize'] = (10.0, 8.0)
 
 
@@ -27,13 +28,15 @@ def norm2(x, y):
 def get_song_info(h5):
     print '%s - %s | (%s) | %s bpm' % (hdf5_getters.get_artist_name(h5), hdf5_getters.get_title(h5), hdf5_getters.get_year(h5), hdf5_getters.get_tempo(h5))
 
+def pickle_this(data, filename):
+    pickle.dump(data, open(filename, 'wb'))    
 
 # In[3]:
 
-pitches = read_pitches()
-n = 3
-h5sample = sample(pitches, n)
 
+h5sample = read_sample()
+n = len(h5sample)
+# print 'n:', n
 # In[6]:
 
 # print [ [(i,j,dtw(x, y, dist=norm2) for i, x in enumerate(h5sample)] for j, y in enumerate(h5sample) if i != j ]
@@ -42,8 +45,8 @@ distance_matrix = np.zeros((n,n))
 
 for i in xrange(n):
     for j in xrange(i, n):
-        print '(%s,%s)' % (i,j)
+        # print '(%s,%s)' % (i,j)
         distance_matrix[i][j] = dtw(h5sample[i], h5sample[j], norm2)
 
-print np.shape(distance_matrix)
-print distance_matrix
+# print np.shape(distance_matrix)
+pickle_this(distance_matrix, 'distance_matrix.p')
--- extract_chroma.py
+++ extract_chroma.py
@@ -2,7 +2,7 @@ import os
 import sys
 import hdf5_getters
 import cPickle as pickle
-
+from random import sample
 
 def dump_pitches(rootdir = './data/', filename = 'pitches.p'):
     h5list = []
@@ -16,14 +16,19 @@ def dump_pitches(rootdir = './data/', filename = 'pitches.p'):
                 h5list[-1].close()
     del h5list
 
-    pickle.dump( pitches, open( filename, "wb" ) )
+    pickle.dump( pitches, open( filename, 'wb' ) )
 
 def read_pitches(filename = 'pitches.p'):
     return pickle.load( open( 'pitches.p', 'rb' ) )
 
+def dump_sample(n = 100, filename = 'sample_pitches.p'):
+    pickle.dump(sample(read_pitches(), n), open (filename, 'wb'))
+
+def read_sample(filename = 'sample_pitches.p'):
+    return pickle.load( open( 'sample_pitches.p', 'rb' ) )
 
 
-
-
+###########################################################
 if __name__ == '__main__':
-    dump_pitches()
+#    dump_pitches()
+    dump_sample()
